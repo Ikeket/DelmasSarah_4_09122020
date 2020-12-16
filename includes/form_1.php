5ad8017464916b0e516265c58ad4bcffbd3ad8da
Ikeket <ikescd@gmail.com>
2020-12-16 21:58:38 +0100

    :mag: (refactor) change indentation on page2.html (renammed : contact.html)

--- includes/form_1.php
+++ includes/form_1.php
@@ -12,8 +12,8 @@
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from your website";
-	$email_body = "You have received a new message. \n\n".
+	$email_subject = "Message de votre site web";
+	$email_body = "Vous avez reçu un nouveau mail. \n\n".
 				"Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
